cb187e7315d71a8d14061a63e1d9f9ddbe59305f
Anekdotin <edwin.eames214@gmail.com>
2020-12-01 16:55:55 -0500

    Initial Commit

--- main.py
+++ main.py
@@ -104,6 +104,20 @@ def selenium_getter():
 
     return driver
 
+def my_proxy(PROXY_HOST, PROXY_PORT):
+    fp = webdriver.FirefoxProfile()
+    # Direct = 0, Manual = 1, PAC = 2, AUTODETECT = 4, SYSTEM = 5
+    print
+    PROXY_PORT
+    print
+    PROXY_HOST
+    fp.set_preference("network.proxy.type", 1)
+    fp.set_preference("network.proxy.http", PROXY_HOST)
+    fp.set_preference("network.proxy.http_port", int(PROXY_PORT))
+    fp.set_preference("general.useragent.override", "whater_useragent")
+    fp.update_preferences()
+    return webdriver.Firefox(firefox_profile=fp)
+
 
 def main():
     if proxy_enabled is True:
